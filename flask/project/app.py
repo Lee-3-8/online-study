348059529c49bd364e554866b070da5955bc173f
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-01 22:05:48 +0900

    flask_10[5/15]

--- flask/project/app.py
+++ flask/project/app.py
@@ -4,6 +4,8 @@ from flask import request
 from flask import redirect
 from flask import render_template
 from models import db
+from flask_wtf.csrf import CSRFProtect
+from forms import RegisterForm
 
 from models import Fcuser
 
@@ -11,6 +13,7 @@ app = Flask(__name__)
 
 @app.route('/register', methods = ['GET','POST'])
 def register():
+    form = RegisterForm()
     if request.method == 'POST':
         userid = request.form.get('userid')
         username = request.form.get('username')
@@ -25,8 +28,9 @@ def register():
 
             db.session.add(fcuser) #db에 넣는 부분
             db.session.commit() #자동으로 하게 설정해놧지만 습관들이자
+
             return redirect('/')
-    return render_template('register.html')
+    return render_template('register.html',form=form)
 
 
 @app.route('/')
@@ -41,7 +45,10 @@ if __name__ == "__main__":
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True # teardown은 사용자 요청의 전달완료를 commit을 하겠다는 뜻
     #commit은 내가 변경한사항이 데이터베이스에 반영되게함
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'qewoijoaisdjfoi'
 
+    csrf = CSRFProtect()
+    csrf.init_app(app)
     db.init_app(app)
     db.app = app
     # db = SQLAlchemy(app)#db를 사용해서 데이터와 관련된 모든것을함
