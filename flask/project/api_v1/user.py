703179b35536d29c060b1b6a68a9dc4d4278bf03
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-05 04:29:50 +0900

    크롤러

--- flask/project/api_v1/user.py
+++ flask/project/api_v1/user.py
@@ -1,6 +1,9 @@
 from flask import jsonify
 from flask import request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 from models import Fcuser, db
+
 from . import api
 
 @api.route('/users', methods=['GET','POST']) #모든사용자 get
@@ -59,3 +62,29 @@ def user_detail(uid):
 	Fcuser.query.filter(Fcuser.id == uid).update(updated_data)
 	user = Fcuser.query.filter(Fcuser.id == uid).first()
 	return jsonify(user.serialize)
+
+@api.route('/crawler', methods=['GET'])
+def crawler():
+	input_num = '110'
+	f = open("G:/도서목록.txt",'w')
+	for i in range(11159,11200):
+		for j in [2,4,6,8]:
+			for k in [1,5]:
+				index = str(i) + str(j) + str(k)
+				print("     "+ index)
+				html = urlopen("https://www.nl.go.kr/NL/search/marc_view.do?viewKey="+index+"&viewType=AH1")
+				bsObject = BeautifulSoup(html, "html.parser")
+				flag = False
+				for link in bsObject.find_all('tr'):
+					a = str(link.find('td'))
+					b = a[4:7]
+					if(b == '001'):
+						c = str(link.select(".left"))[18:30]
+					if(b == input_num):
+						flag = True
+				if(flag == True):
+					f.write(c+"\n")
+					print(c)
+	f.close()
+	return jsonify({})
+
