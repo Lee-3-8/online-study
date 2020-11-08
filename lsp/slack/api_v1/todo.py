ac23cf96dd2625c1ba2ae6f7ce92e176adb66a52
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-09 00:46:37 +0900

    추가 오류수정

--- lsp/slack/api_v1/todo.py
+++ lsp/slack/api_v1/todo.py
@@ -1,55 +1,54 @@
 from flask import jsonify
 from flask import request
 from flask import Blueprint
-from models import Todo,db
+from models import Todo, db
 import datetime
 import requests
 from . import api
 
 
 def send_slack(msg):
-	res = requests.post(
-			"https://hooks.slack.com/services/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",
-			json={"text": "Hello world"},
-			headers={"Content-Type": "application/json"})
+    res = requests.post(
+        "https://hooks.slack.com/services/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",
+        json={'text': msg},
+        headers={"Content-Type": "application/json"})
 
 
-
-@api.route("/todos", methods=["Get", "POST"])
+@api.route("/todos", methods=["GET", "POST"])
 def todos():
-	if request.method == "POST":
-		# 생성 코드
-		send_slack('Todo가 생성됨')
-	elif request.method == "GET":
-		pass
+    if request.method == "POST":
+        # 생성 코드
+        send_slack("Todo가 생성됨")
+    elif request.method == "GET":
+        pass
 
-	data = request.get_json()
-	return jsonify(data)
+    data = request.get_json()
+    return jsonify(data)
 
 
 @api.route("/slack/todos", methods=["POST"])
 def slack_todos():
 
-	res = request.form["text"].split(' ')
-	cmd, *args = res
+    res = request.form["text"].split(' ')
+    cmd, *args = res
 
-	ret_msg = ''
+    ret_msg = ""
 
-	if cmd == 'create':
-		todo_name = args[0]
+    if cmd == "create":
+        todo_name = args[0]
 
-		todo = Todo()
-		todo.title = todo_name
+        todo = Todo()
+        todo.title = todo_name
 
-		db.session.add(todo)
-		db.session.commit()
-		ret_msg = 'todo가 생성되었습니다.'
+        db.session.add(todo)
+        db.session.commit()
+        ret_msg = "todo가 생성되었습니다."
 
-		send_slack('[%s] %s'%(str(datetime.datetime.now()),todo_name))
+        send_slack("[%s] %s" % (str(datetime.datetime.now()), todo_name))
 
-	elif cmd == 'list':
-		todos = Todo.query.all()
-		for idx, todo in enumerate(todos):
-			ret_msg += '%d. %s (~%s)\n'%(idx+1, todo.title, str(todo.tstamp))
+    elif cmd == "list":
+        todos = Todo.query.all()
+        for idx, todo in enumerate(todos):
+            ret_msg += "%d. %s (~ %s)\n" % (idx + 1, todo.title, str(todo.tstamp))
 
-	return jsonify(ret_msg)
+    return ret_msg
