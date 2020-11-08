37d6b228394457cda310c71925b2457d4551bc85
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-08 23:58:43 +0900

    slack command 조회만들기 [15/15]

--- lsp/slack/api_v1/todo.py
+++ lsp/slack/api_v1/todo.py
@@ -2,18 +2,24 @@ from flask import jsonify
 from flask import request
 from flask import Blueprint
 from models import Todo,db
+import datetime
 import requests
 from . import api
+
+
+def send_slack(msg):
+	res = requests.post(
+			"https://hooks.slack.com/services/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",
+			json={"text": "Hello world"},
+			headers={"Content-Type": "application/json"})
+
 
 
 @api.route("/todos", methods=["Get", "POST"])
 def todos():
 	if request.method == "POST":
-		res = requests.post(
-			"https://hooks.slack.com/services/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",
-			json={"text": "Hello world"},
-			headers={"Content-Type": "application/json"},
-		)
+		# 생성 코드
+		send_slack('Todo가 생성됨')
 	elif request.method == "GET":
 		pass
 
@@ -38,6 +44,8 @@ def slack_todos():
 		db.session.add(todo)
 		db.session.commit()
 		ret_msg = 'todo가 생성되었습니다.'
+
+		send_slack('[%s] %s'%(str(datetime.datetime.now()),todo_name))
 
 	elif cmd == 'list':
 		todos = Todo.query.all()
