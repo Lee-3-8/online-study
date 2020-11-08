b70eb61e1f3d97c42660e92f26a0bdf714a486e5
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-08 23:42:30 +0900

    slash command [13/15]

--- lsp/slack/api_v1/todo.py
+++ lsp/slack/api_v1/todo.py
@@ -1,28 +1,45 @@
 from flask import jsonify
 from flask import request
 from flask import Blueprint
+from models import Todo,db
 import requests
 from . import api
 
 
 @api.route("/todos", methods=["Get", "POST"])
 def todos():
-    if request.method == "POST":
-        res = requests.post(
-            "https://hooks.slack.com/services/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",
-            json={"text": "Hello world"},
-            headers={"Content-Type": "application/json"},
-        )
-    elif request.method == "GET":
+	if request.method == "POST":
+		res = requests.post(
+			"https://hooks.slack.com/services/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",
+			json={"text": "Hello world"},
+			headers={"Content-Type": "application/json"},
+		)
+	elif request.method == "GET":
+		pass
+
+	data = request.get_json()
+	return jsonify(data)
 
 
-    data = request.get_json()
-    return jsonify(data)
+@api.route("/slack/todos", methods=["POST"])
+def slack_todos():
 
+	res = request.form["text"].split(' ')
+	cmd, *args = res
 
-@api.route("/test", methods=["POST"])
-def test():
-    res = request.form["text"]
-    print(res)
+	ret_msg = ''
 
-    return jsonify(res)
+	if cmd == 'create':
+		todo_name = args[0]
+
+		todo = Todo()
+		todo.title = todo_name
+
+		db.session.add(todo)
+		db.session.commit()
+		ret_msg = 'todo가 생성되었습니다.'
+
+	elif cmd == 'list':
+		pass
+
+	return jsonify(res)
