1442daefb0997bea37614e21ac98c4e6cd28890c
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-11 19:41:44 +0900

    slack 로그인 [1/15]

---
+++ lsp/slack/forms.py
@@ -0,0 +1,9 @@
+from models improt Fcuser
+from flask_wtf import FlaskForm
+from wtforms import StringField , PasswordField
+from wtforms.validators import DataRequired, EqualTo
+
+class RegisterForm(FlaskForm):
+	userid = StringField('userid', validators=[DataRequired()])
+	password = StringField('password', validators=[DataRequired()])
+	repassword = StringField('repassword', validators=[DataRequired()])
