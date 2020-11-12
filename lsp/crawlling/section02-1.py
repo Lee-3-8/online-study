8bee0f46dd066d58a70793613a4de36ebf5fb7fa
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-13 01:24:21 +0900

    urllib 사용법 2 [8/15]

--- lsp/crawlling/section02-1.py
+++ lsp/crawlling/section02-1.py
@@ -1,8 +1,22 @@
-#파이썬 크롤링 기초
+# 파이썬 크롤링 기초
 # urllib 사용법
-import urllib.request as request
+import urllib.request as req
 
 
 # 파일 url
-img_url = 'https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/4arX/image/ThmomYfV2fLl1k3iXCe__ZywsAk.jpg'
-html_url = ''
+img_url = "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/4arX/image/ThmomYfV2fLl1k3iXCe__ZywsAk.jpg"
+html_url = "http://google.com"
+
+save_path1 = "G:/test1.jpg"
+save_path2 = "G:/index.html"
+
+try:
+    file1, header1 = req.urlretrieve(img_url, save_path1)
+    file2, header2 = req.urlretrieve(html_url, save_path2)
+except Exception as e:
+    print("Download failed")
+    print(e)
+else:
+    # Header 정보 출력
+    print(header1)
+    print(header2)
