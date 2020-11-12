1de39a8f4f110c4baf856dc74c4cd3ff234448da
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-13 01:09:35 +0900

    urllib사용법 (1) [7/15]

---
+++ lsp/crawlling/section02-1.py
@@ -0,0 +1,8 @@
+#파이썬 크롤링 기초
+# urllib 사용법
+import urllib.request as request
+
+
+# 파일 url
+img_url = 'https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/4arX/image/ThmomYfV2fLl1k3iXCe__ZywsAk.jpg'
+html_url = ''
