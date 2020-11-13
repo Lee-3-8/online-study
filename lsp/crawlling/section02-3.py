218a1edd90350a195a0c9471e10f0ad89c9cc611
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-14 02:04:57 +0900

    lxml 사용 기초 스크랩핑 (3) [12/15]

--- lsp/crawlling/section02-3.py
+++ lsp/crawlling/section02-3.py
@@ -14,23 +14,23 @@ def main():
 
 	#신문사 링크 리스트 획득
 	urls = scrape_news_list_page(response)
-
-	# #결과 출력
-	# for url in urls:
-	# 	#url 출력
-	# 	print(url)
-	# 	#파일 쓰기
+	#결과 출력
+	for url in urls:
+		#url 출력
+		print(url)
+		#파일 쓰기
 
 def scrape_news_list_page(response):
 	# URL 리스트 선언
 	urls = []
-
 	#태그 정보 문자열 저장
 	root = lxml.html.fromstring(response.content)
-
-	for a in root.cssselect(''):
+	#공백이 하위 api_list->api.item->a->api_link , .thumb_box _NM_NEWSSTAND_THUMB _NM_NEWSSTAND_THUMB_press_valid .popup_wrap  a.btn_popup
+	for a in root.cssselect('.thumb_area .btn_popup'):
 		#링크
-		pass
+		url = a.get('href')
+		urls.append(url)
+	return urls
 
 # 스크랩핑 시작
 if __name__ == "__main__":
