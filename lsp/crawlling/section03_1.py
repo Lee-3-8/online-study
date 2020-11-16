8f8cd6b39c92464779769d8ecb2b7e375d168b98
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-16 21:15:21 +0900

    get 방식 데이터 통신 2 [1/15]

--- lsp/crawlling/section03_1.py
+++ lsp/crawlling/section03_1.py
@@ -5,16 +5,39 @@
 import urllib.request
 from urllib.parse import urlparse
 
-#기본 요청1(encar)
-url = "http://www.encar.com/"
+# #기본 요청1(encar)
+# url = "http://www.encar.com/"
 
-mem = urllib.request.urlopen(url)
+# mem = urllib.request.urlopen(url)
 
-# 여러 정보
-print('type : {}'.format(type(mem)))
-print('geturl : {}'.format(mem.geturl()))
-print('status : {}'.format(mem.status))
-print('header : {}'.format(mem.getheaders()))
-print('getcode : {}'.format(mem.getcode()))
-print('getcode : {}'.format(mem.read(100).decode('utf-8')))
-print('parse : {}'.format(urlparse('http://www.encar.com?test=test').query))
+# # 여러 정보
+# print('type : {}'.format(type(mem)))
+# print('geturl : {}'.format(mem.geturl()))
+# print('status : {}'.format(mem.status))
+# print('header : {}'.format(mem.getheaders()))
+# print('getcode : {}'.format(mem.getcode()))
+# print('getcode : {}'.format(mem.read(100).decode('utf-8')))
+# print('parse : {}'.format(urlparse('http://www.encar.com?test=test').query))
+
+#기본 요청2(ipify)
+API = 'https://api.ipify.org'
+
+# Get 방식 parameter
+values = {
+	'format' : 'json' #text,  json , jsonp
+}
+
+print('before param : {}'.format(values))
+params = urllib.parse.urlencode(values)
+print('after param : {}'.format(params))
+
+# 요청 URL 생성
+URL = API + "?" + params
+print("요청 URL = {}".format(URL))
+
+#수신 데이터 읽기
+data = urllib.request.urlopen(URL).read()
+
+# 수신 데이터 디코딩
+text = data.decode('UTF-8')
+print('response : {}'.format(text))
