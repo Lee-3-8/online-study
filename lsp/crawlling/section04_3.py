e9dc2ac858da1216d41f4241509a07b86e7839f3
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-17 02:32:33 +0900

    requset 사용 c-2 [11/15]

--- lsp/crawlling/section04_3.py
+++ lsp/crawlling/section04_3.py
@@ -10,18 +10,18 @@
 # PUT www.movies.com/movies/ 영화를 수정
 # DELETE www.movies.com/movies/ 영화를 삭제
 
-import requests as req
+import requests
 # 세션 활성화
-s = req.Session()
+s = requests.Session()
 
 # 예제1
-r = s.get('https//api.github.com/events')
+r = s.get('https://api.github.com/events')
 
 # 수신상태  체크
 r.raise_for_status()
 
 #출력
-print(r.text)
+# print(r.text)
 
 
 #예제2
@@ -31,4 +31,50 @@ jar = requests.cookies.RequestsCookieJar()
 
 #쿠키 삽입
 
-jar.set('name' , 'niceman' , domain="httpbin.org", path='/c')
+jar.set('name' , 'niceman' , domain="httpbin.org", path='/cookies')
+
+# 요청
+
+r = s.get('https://httpbin.org/cookies', cookies=jar)
+
+#출력
+# print(r.text)
+
+# 예제 3
+r = s.get("https://github.com",timeout=5)
+
+#출력
+# print(r.text)
+
+
+#예제 4
+r = s.post("https://httpbin.org/post",data={'id':'test77' , 'pw' : '111'}, cookies=jar)
+
+#출력
+# print(r.text)
+# print(r.headers)
+
+# 예제 5
+# 요청
+test_url = "https://httpbin.org/"
+
+payload1 = {'id':'test7787' , 'pw' : '11111'}
+payload2 = (('id','test77') , ('pw','111')) #튜플로도가능
+
+r = s.post(test_url + "post", data = payload1)
+
+#출력
+# print(r.text)
+
+#예제6 (PUT)
+r = s.put(test_url + "put" , data=payload1)
+
+#출력
+# print(r.text)
+
+#예제 7
+
+r = s.delete(test_url + "delete" , data={'id' : 1})
+
+#출력
+print(r.text)
