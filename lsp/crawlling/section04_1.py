26cf1217241d5c0e000335ae019ebaf3159a10ef
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-16 23:22:22 +0900

    request 사용 스크랩핑 a-2 [7/15]

--- lsp/crawlling/section04_1.py
+++ lsp/crawlling/section04_1.py
@@ -11,20 +11,51 @@ requests 모듈에 대해서배움 (1) -session
 """
 import requests
 
-#세션 활성화
+# #세션 활성화
+# s = requests.Session()
+# r = s.get('https://www.naver.com/')
+
+# #수신 데이터
+# # print(r.text)
+
+# #수신 상태 코드
+# print('Status Code : {}'.format(r.status_code))
+
+
+# # 확인
+# print('OK? : {}'.format(r.ok)) # True 반환
+
+
+# # 세션 열면 닫아줘야함~(with 문도 있음 )
+# s.close()
+
+
 s = requests.Session()
-r = s.get('https://www.naver.com/')
 
-#수신 데이터
-# print(r.text)
+# 쿠키 return
+r1 = s.get('https://httpbin.org/cookies', cookies = {'name' : 'kim1'})
+print(r1.text)
 
-#수신 상태 코드
-print('Status Code : {}'.format(r.status_code))
+# 쿠키 set -> 서버에 쿠키정보 저장
 
+r2 = s.get('https://httpbin.org/cookies/set', cookies = {'name' : 'kim2'})
+print(r2.text)
 
-# 확인
-print('OK? : {}'.format(r.ok)) # True 반환
+# User-Agent
+url = "https://httpbin.org"
+headers = {'user-agent': 'nice-man_1.0.0_win10_ram16_home_chrome'}
 
+#Header 정보 전송
+r3 = s.get(url,headers=headers) # 인자에 쿠키도 넣어서 보낼 수 있다.
 
-# 세션 열면 닫아줘야함~(with 문도 있음 )
 s.close()
+
+# 실무에서 with문을 사용 권장함 -> 파일 ,DB,HTTP , 가독성이좋고 실수도줄일 수 있다.
+ with requests.Session() as s:
+ 	r = s.get('http://finance.daum.net')
+ 	print(r.text)
+ 	print(r.ok)
+
+
+
+
