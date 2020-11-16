198907b66e739d20fb73b6ec59c1a5134618e20f
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-17 00:45:37 +0900

    request 사용 스크랩핑 b-2 [9/15]

--- lsp/crawlling/section04_2.py
+++ lsp/crawlling/section04_2.py
@@ -8,14 +8,36 @@ requests 모듈에 대해서배움 (2) - json
 import json
 import requests
 
-with requests.Session() as s:
-    r = s.get("https://httpbin.org/stream/100", stream=True)  # 텍스트로 불러올땐 stream  true로
-#수신 확인
-    print(r.text)
+s = requests.Session()
 
-# encoding 확인
-print('Encoding : {}'.format(r.encoding))
-if r.encoding is None:
-	r.encoding	= 'UTF-8'
+# r = s.get("https://httpbin.org/stream/100", stream=True)  # 텍스트로 불러올땐 stream  true로
+# # 수신 확인
+# print(r.text)
+# # encoding 확인
+# print('Encoding : {}'.format(r.encoding))
+# if r.encoding is None:
+# 	r.encoding	= 'UTF-8'
+# print('After Encoding: {}'.format(r.encoding))
+# for line in r.iter_lines(decode_unicode=True):
+# 	# 라인 출력 후 타입 확인
+# 	# print(line)
+# 	# print(type(line))
+# 	#json(Dict) 변환 후 타입 확인
+# 	b = json.loads(line)
+# 	print(b)
+# 	print(type(b))
+# 	# 정보 내용 출력
+# 	for k,v in b.items():
+# 		print("Key: {} , Value : {}".format(k,v))
 
-print('After Encoding: {}'.format(r.encoding))
+r = s.get("https://jsonplaceholder.typicode.com/todos/1")
+
+print(r.headers)
+print(r.text)
+print(r.json().keys())
+print(r.json().values())
+print(r.encoding)
+# 바이너리 정보
+print(r.content)
+
+s.close()
