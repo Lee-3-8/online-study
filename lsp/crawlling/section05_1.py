a9353f67c3d70fa445b89100079fc321f8713c32
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-17 02:49:21 +0900

    Beautiful Soup a-1 [13/15]

--- lsp/crawlling/section05_1.py
+++ lsp/crawlling/section05_1.py
@@ -1,5 +1,5 @@
 # section05-1
-# beautifulsoup 사용 스크래핑 1
+# beautifulsoup 사용 스크래핑 2
 
 from bs4 import BeautifulSoup
 
@@ -24,3 +24,32 @@ html = """
 </html>
 """
 
+#예제1 bs4 기초
+soup = BeautifulSoup(html,'html.parser')
+
+#타입확인
+# print('soup', type(soup))
+# print('prettify', soup.prettify())
+
+#h1 태그 접근
+h1 = soup.html.body.h1
+# print('h1',h1)
+
+#p 태그 접근 여러개 있으면 가장 첫번째를 가져옴
+p1 = soup.html.body.p
+# print('p1',p1)
+
+#다음 태그
+#p->공백->그다음 , next는 그닥 많이안쓰고 알아만 두자
+p2 = p1.next_sibling.next_sibling
+# print('p2',p2)
+
+# #텍스트 출력
+# print('h1 >>',h1.string)
+# print('p1 >>',p1.string)
+
+# #함수확인
+# print(dir(p2))
+
+#다음 엘리먼트 확인 / 잘사용안하고 string으로함
+print(p2.next_element) # print(list(p2.next_element)
