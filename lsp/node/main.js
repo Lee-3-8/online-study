0e4f3d912be790e6b8c122eb46e739d620a26759
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-28 02:54:37 +0900

    첫 서버 오픈하기 [2/15]

--- lsp/node/main.js
+++ lsp/node/main.js
@@ -1,1 +1,19 @@
-console.log("Hello, Wolrd");
+
+const http  = require("http");
+http.createServer(function(request,response){
+	/*
+	HTTP 헤더 전송
+	HTTP status : 200 : ok
+	content type: text/plain
+	*/
+	response.writeHead(200,{'Content-Type' : 'text/plain'});
+
+	/*
+		Response Body 를 "Heloo World"으로 설정
+
+	*/
+
+	response.end("Hello.World\n");
+}).listen(8081);
+
+console.log("server running at http://127.0.0.1:8081");
