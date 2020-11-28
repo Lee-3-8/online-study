3edc6cab83d3a0f7e9134caed2bef25a30267903
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-11-29 02:05:34 +0900

    html layout [8/15]

--- lsp/node/route.js
+++ lsp/node/route.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();//라우터 분리
 
+
 router.get('/',(req,res)=>{
-	res.sendFile(path.join(__dirname,'templates','test.html'));
+	res.sendFile(path.join(__dirname,'public','test.html'));
 });
 
 router.get('/user/:name', (req,res)=>{
