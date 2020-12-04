e45e7f48de30d2bde85426bbb078efa29b6158b1
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-12-04 13:47:28 +0900

    js 함수 [4/15]

--- lsp/Js/test.js
+++ lsp/Js/test.js
@@ -20,3 +20,18 @@
 do{
 	console.log("먼저실행하고 조건 비교 ")
 }while(false)
+
+const func = function(){
+	console.log("dfdf")
+}
+
+function Func(a , b , c){
+	this.name = a;
+	this.color = b;
+	console.log("dfdf")
+	const move = c;
+}
+
+const inst = new Func("아","ㄱㅊ","오류날껄 ")
+
+console.log(inst.name , inst.color , a.move) // => 오류
