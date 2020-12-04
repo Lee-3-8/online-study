ed029fa53a278c96d6afd73a432f838930cdd0b1
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-12-04 13:40:49 +0900

    js 3강 반복문 [3/15]

--- lsp/Js/test.js
+++ lsp/Js/test.js
@@ -1,17 +1,22 @@
-const a = ["hello" , 20, "nodejs" , "2222"]
+// const a = ["hello" , 20, "nodejs" , "2222"]
 
-console.log(a[0] , a[3])
+// console.log(a[0] , a[3])
 
-a[4] = 55
+// a[4] = 55
 
-console.log(a[4])
+// console.log(a[4])
 
-console.log(a.length)
-console.log(a.indexOf("hello"))
+// console.log(a.length)
+// console.log(a.indexOf("hello"))
 
-const b = new Array("2222","nodejs") // 비추천
-const c = []
+// const b = new Array("2222","nodejs") // 비추천
+// const c = []
 
-for(var i = 0 ; i<5; i++){
-	console.log(i)
-}
+// for(var i = 0 ; i<5; i++){
+// 	console.log(i)
+// }
+
+
+do{
+	console.log("먼저실행하고 조건 비교 ")
+}while(false)
