e6045eccfdef427a59b715c575a9879da64dd3c3
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-12-04 13:34:07 +0900

    js 2강 [2/15]

---
+++ lsp/Js/test.js
@@ -0,0 +1,17 @@
+const a = ["hello" , 20, "nodejs" , "2222"]
+
+console.log(a[0] , a[3])
+
+a[4] = 55
+
+console.log(a[4])
+
+console.log(a.length)
+console.log(a.indexOf("hello"))
+
+const b = new Array("2222","nodejs") // 비추천
+const c = []
+
+for(var i = 0 ; i<5; i++){
+	console.log(i)
+}
