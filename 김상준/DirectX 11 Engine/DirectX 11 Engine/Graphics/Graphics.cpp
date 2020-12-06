a8cf2fa7fce6688734b6e44762dbc2301f529f40
sang-jun-kim <59325814+sang-jun-kim@users.noreply.github.com>
2020-12-06 18:36:32 +0900

    DirectX Using ImGui [9/10]

--- 김상준/DirectX 11 Engine/DirectX 11 Engine/Graphics/Graphics.cpp
+++ 김상준/DirectX 11 Engine/DirectX 11 Engine/Graphics/Graphics.cpp
@@ -48,7 +48,8 @@ void Graphics::RenderFrame()
 	UINT offset = 0;
 	
 	// Update Constant Buffer
-	DirectX::XMMATRIX world = DirectX::XMMatrixIdentity();
+	static float translationOffset[3] = { 0, };
+	DirectX::XMMATRIX world = DirectX::XMMatrixTranslation(translationOffset[0], translationOffset[1], translationOffset[2]);
 	constantBuffer.data.mat = world * camera.GetViewMatrix() * camera.GetProjectionMatrix();
 	constantBuffer.data.mat = DirectX::XMMatrixTranspose(constantBuffer.data.mat);
 	if (!constantBuffer.ApplyChanges())
@@ -77,12 +78,22 @@ void Graphics::RenderFrame()
 	spriteFont->DrawString(spriteBatch.get(), StringConverter::StringToWide(fpsString).c_str(), DirectX::XMFLOAT2(0, 0), DirectX::Colors::White, 0.0f, DirectX::XMFLOAT2(0.5f, 0.5f), DirectX::XMFLOAT2(1.0f, 1.0f));
 	spriteBatch->End();
 
+	static int counter = 0;
 	// Start Dear ImGui Frame
 	ImGui_ImplDX11_NewFrame();
 	ImGui_ImplWin32_NewFrame();
 	ImGui::NewFrame();
 	// Create ImGui Test Window
 	ImGui::Begin("Test");
+	ImGui::Text("This is example text.");
+	if (ImGui::Button("CLICK ME!"))
+	{
+		counter += 1;
+	}
+	ImGui::SameLine();
+	std::string clickCount = "Click Count: " + std::to_string(counter);
+	ImGui::Text(clickCount.c_str());
+	ImGui::DragFloat3("Translation X/Y/Z", translationOffset, 0.1f, -5.0f, 5.0f);
 	ImGui::End();
 	// Assemble Together Draw Data
 	ImGui::Render();
