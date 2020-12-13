fe5980bf846796d18b33dd4fba97cfedff87a77c
sang-jun-kim <59325814+sang-jun-kim@users.noreply.github.com>
2020-12-13 22:37:46 +0900

    DirectX Moving Model [6/10]

--- 김상준/DirectX 11 Engine/DirectX 11 Engine/Graphics/Camera.cpp
+++ 김상준/DirectX 11 Engine/DirectX 11 Engine/Graphics/Camera.cpp
@@ -83,6 +83,13 @@ void Camera::SetPosition(const XMVECTOR& pos)
 	this->UpdateViewMatrix();
 }
 
+void Camera::SetPosition(const XMFLOAT3& pos)
+{
+	this->pos = pos;
+	this->posVector = XMLoadFloat3(&this->pos);
+	this->UpdateViewMatrix();
+}
+
 void Camera::SetPosition(float x, float y, float z)
 {
 	this->pos = XMFLOAT3(x, y, z);
@@ -94,6 +101,15 @@ void Camera::AdjustPosition(const XMVECTOR& pos)
 {
 	this->posVector += pos;
 	XMStoreFloat3(&this->pos, this->posVector);
+	this->UpdateViewMatrix();
+}
+
+void Camera::AdjustPosition(const XMFLOAT3& pos)
+{
+	this->pos.x += pos.x;
+	this->pos.y += pos.y;
+	this->pos.z += pos.z;
+	this->posVector = XMLoadFloat3(&this->pos);
 	this->UpdateViewMatrix();
 }
 
@@ -113,6 +129,13 @@ void Camera::SetRotation(const XMVECTOR& rot)
 	this->UpdateViewMatrix();
 }
 
+void Camera::SetRotation(const XMFLOAT3& rot)
+{
+	this->rot = rot;
+	this->rotVector = XMLoadFloat3(&this->rot);
+	this->UpdateViewMatrix();
+}
+
 void Camera::SetRotation(float x, float y, float z)
 {
 	this->rot = XMFLOAT3(x, y, z);
@@ -124,6 +147,15 @@ void Camera::AdjustRotation(const XMVECTOR& rot)
 {
 	this->rotVector += rot;
 	XMStoreFloat3(&this->rot, this->rotVector);
+	this->UpdateViewMatrix();
+}
+
+void Camera::AdjustRotation(const XMFLOAT3& rot)
+{
+	this->rot.x += rot.x;
+	this->rot.y += rot.y;
+	this->rot.z += rot.z;
+	this->rotVector = XMLoadFloat3(&this->pos);
 	this->UpdateViewMatrix();
 }
 
--- 김상준/DirectX 11 Engine/DirectX 11 Engine/Graphics/Graphics.h
+++ 김상준/DirectX 11 Engine/DirectX 11 Engine/Graphics/Graphics.h
@@ -17,6 +17,7 @@ public:
 	bool Initialize(HWND hWnd, int width, int height);
 	void RenderFrame();
 	Camera camera;
+	Model model;
 
 private:
 	int windowWidth = 0;
@@ -35,8 +36,6 @@ private:
 	PixelShader pixelshader;
 	ConstantBuffer<CB_VS_vertexshader> cb_vs_vertexshader;
 	ConstantBuffer<CB_PS_pixelshader> cb_ps_pixelshader;
-
-	Model model;
 
 	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> depthStencilView;
 	Microsoft::WRL::ComPtr<ID3D11Texture2D> depthStencilBuffer;
