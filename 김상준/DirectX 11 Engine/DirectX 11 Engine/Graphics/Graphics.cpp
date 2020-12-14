99739a1315f4a6825cd6035f046eacb5490a42d3
sang-jun-kim <59325814+sang-jun-kim@users.noreply.github.com>
2020-12-15 00:43:39 +0900

    DirectX Color/Texture Class [10/10]

--- 김상준/DirectX 11 Engine/DirectX 11 Engine/Graphics/Graphics.cpp
+++ 김상준/DirectX 11 Engine/DirectX 11 Engine/Graphics/Graphics.cpp
@@ -309,7 +309,7 @@ bool Graphics::InitializeScene()
 		COM_ERROR_IF_FAILED(hr, "Failed to initialize constant buffer.");
 
 		// Initialize Model(s)
-		if (!gameObject.Initialize("Data/Objects/nanosuit/nanosuit.obj" ,this->device.Get(), this->deviceContext.Get(), this->grassTexture.Get(), cb_vs_vertexshader))
+		if (!gameObject.Initialize("Data/Objects/nanosuit/nanosuit.obj" ,this->device.Get(), this->deviceContext.Get(), cb_vs_vertexshader))
 		{
 			return false;
 		}
