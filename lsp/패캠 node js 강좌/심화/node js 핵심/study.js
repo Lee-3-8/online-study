0a16e8b677de556aa5f320f9edd5a6393e1f27ae
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-12-18 14:14:00 +0900

    14-6 node js vs browser [9/15]

---
+++ lsp/패캠 node js 강좌/심화/node js 핵심/study.js
@@ -0,0 +1,20 @@
+//node js 개발
+window
+/*
+윈도우 객체 -> 전역객체 nodejs에서는 지원안함
+윈도우객체는 웹 브라우저에서만 작동 
+*/
+
+const dns = require('dns');
+
+import React from 'react' //es6이후 문법 
+/*
+프론트에서는 import로 사용하지만 , node에서는 require을 사용하여 패키지를 불러온다 
+*/
+
+const a
+let b
+var c
+/*
+스코프 차이가 있다. 
+*/
