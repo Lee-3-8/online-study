a7e2670dbe5a6a4fc1c26ef61b37bed31cd76295
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-12-22 21:19:34 +0900

    14.16 find , includes [4/15]

--- lsp/패캠 node js 강좌/심화/node js 핵심/fast.js
+++ lsp/패캠 node js 강좌/심화/node js 핵심/fast.js
@@ -31,8 +31,21 @@
 
 // module.exports = {edit , write}
 
-const arr  =[2,3,4]
-const isBiggerthanOne = arr.every(key => key > 1)
-//array에서 모든조건이 만족할때 실행 위 경우에는 2에서 바로 끝
+////////////////EVERY 문법 /////////////////
+// const arr  =[2,3,4]
+// const isBiggerthanOne = arr.every(key => key > 1)
+// //array에서 모든조건이 만족할때 실행 위 경우에는 2에서 바로 끝
+// 프론트에서도 많이사용됨 
 
-console.log(isBiggerthanOne)
+// console.log(isBiggerthanOne)
+
+
+///////////////find , include//////////
+
+const arr = ['node.js' , '올인원']
+
+const ret = arr.find(key => key == '올인원')//반환 : 올인원 
+const res = arr.includes('node.js');//반환 : true
+console.log(res);
+// 찾아서 실제로 사용할 데이터면 find , 있는지 유무 판단은 includes
+// 두가지를 구분지어 사용해야 데이터 누수를 방지할 수 있다.
