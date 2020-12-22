53c26127f9659341b681126994912dd6493ddff9
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2020-12-22 21:12:29 +0900

    14.15 every [3/15]

--- lsp/패캠 node js 강좌/심화/node js 핵심/fast.js
+++ lsp/패캠 node js 강좌/심화/node js 핵심/fast.js
@@ -1,32 +1,38 @@
 'use strict' //엄격모드 : 컴파일시 좀더 엄격한 문법적용
 
-const obj = {
-	title : 'node js 올인원 패키지'
-}
+// const obj = {
+// 	title : 'node js 올인원 패키지'
+// }
 
 
 
-function isEmptyobj (){
-	if(obj.title){
-		return true
-	}else {
-		return else;
-	}	
-}
+// function isEmptyobj (){
+// 	if(obj.title){
+// 		return true
+// 	}else {
+// 		return else;
+// 	}	
+// }
 
-isEmptyobj()
-
-
+// isEmptyobj()
 
 
-///최신문법으로
-
-const isEmptyobj1 = () => obj.title ? true : false;
-isEmptyobj1()
 
 
-// module.exports 
-function edit(){}
-function write(){}
+// ///최신문법으로
 
-module.exports = {edit , write}
+// const isEmptyobj1 = () => obj.title ? true : false;
+// isEmptyobj1()
+
+
+// // module.exports 
+// function edit(){}
+// function write(){}
+
+// module.exports = {edit , write}
+
+const arr  =[2,3,4]
+const isBiggerthanOne = arr.every(key => key > 1)
+//array에서 모든조건이 만족할때 실행 위 경우에는 2에서 바로 끝
+
+console.log(isBiggerthanOne)
