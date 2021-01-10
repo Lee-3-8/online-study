821aa0360ed0d3b1e0ee1b1a9786cb3528805466
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-01-10 23:26:46 +0900

    14.57 함수형 프로그래밍 (3) [12/15]

--- lsp/패캠_node_js강좌/심화/node_js_핵심/functionalpro.js
+++ lsp/패캠_node_js강좌/심화/node_js_핵심/functionalpro.js
@@ -17,10 +17,16 @@
 
 const arr = ['pdf', 'html', 'html', 'gif', 'gif', 'gif']
 
-const res = numbers.reduce((tot, amt) => {
-  if (amt > 0) tot.push(amt)
-  return tot
-}, []) //두번째 파라미터 []로 tot가 초기화된다.
+// const res = numbers.reduce((tot, amt) => {
+//   if (amt > 0) tot.push(amt)
+//   return tot
+// }, []) //두번째 파라미터 []로 tot가 초기화된다.
+
+// console.log(res)
+
+const res = arr.reduce((cnt, fileType) => {
+  cnt[fileType] = (cnt[fileType] || 0) + 1 //cnt[fileType]에서 undefined가 나오는것을 방지
+  return cnt
+}, {})
 
 console.log(res)
-
