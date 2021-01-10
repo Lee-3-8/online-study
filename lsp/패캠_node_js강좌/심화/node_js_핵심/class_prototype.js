7ca5dc06ed6229625c68b8b423c0216888fe2cd4
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-01-10 22:21:41 +0900

    14.52 class vs prototype (2) [6/15]

--- lsp/패캠_node_js강좌/심화/node_js_핵심/class_prototype.js
+++ lsp/패캠_node_js강좌/심화/node_js_핵심/class_prototype.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// 프로토타입을 사용한 코드
+
 function fullstack (backend, frontend) {
   this.backend = backend
   this.frontend = frontend
@@ -17,3 +19,34 @@ function fullstack (backend, frontend) {
 const Fullstack = new fullstack('js','js')
 console.log(Fullstack.getBackend(), Fullstack.getFrontend()) // js , js 출력
 
+//클래스를 사용한 코드
+
+class Fullstack {
+  constructor(backend , frontend) {
+    //constructor에서는 await 키워드는 사용 불가능함
+    //클래스 내부에는 비동기 코드가 안되는가? -> no!! static factory method 패턴을 사용
+      this.backend = backend
+      this.frontend = frontend
+  }
+
+  getBackend () {
+    return this.backend
+  }
+
+  getFrontend () {
+    return this.frontend
+  }
+
+  setBackend (backend) {
+    this.backend = backend
+  }
+
+  setFrontend (frontend) {
+    this.frontend = frontend
+  } 
+  
+}
+
+const fullstack = new Fullstack('js' , 'js')
+
+// js class를 사용하는것이 실제로 객체지향형 프로그래밍에 더 가깝다.
