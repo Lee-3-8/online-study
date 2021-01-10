ed48411e234d8289ff95de885853e4f07e7b211a
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-01-10 23:21:23 +0900

    14.56 함수형 프로그래밍 (2) [11/15]

--- lsp/패캠_node_js강좌/심화/node_js_핵심/functionalpro.js
+++ lsp/패캠_node_js강좌/심화/node_js_핵심/functionalpro.js
@@ -1,7 +1,26 @@
 'use strict'
 
-const numbers = [10, 20, 30, 40]
+// const numbers = [10, 20, 30, 40]
 
-//reduce를 사용하게 된다면 각 배열의 요소를 접근가능 , 가독성이 높아짐
+// //reduce를 사용하게 된다면 각 배열의 요소를 접근가능 , 가독성이 높아짐
 
-const sum = numbers.reduce
+// const avg = numbers.reduce((tot , val ,idx , arr) => {
+//   tot += val
+//   if (idx === arr.length - 1) {
+//     return tot/arr.length
+//   } else {
+//     return tot
+//   }
+// });
+
+// console.log(avg)
+
+const arr = ['pdf', 'html', 'html', 'gif', 'gif', 'gif']
+
+const res = numbers.reduce((tot, amt) => {
+  if (amt > 0) tot.push(amt)
+  return tot
+}, []) //두번째 파라미터 []로 tot가 초기화된다.
+
+console.log(res)
+
