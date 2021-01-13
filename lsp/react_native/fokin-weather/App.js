3f7327b90b279a0d8efbf59982b5850dff547888
Lee-3-8 <42242359+Lee-3-8@users.noreply.github.com>
2021-01-13 15:40:35 +0900

    Rn - Loading page  [5/15]

--- lsp/react_native/fokin-weather/App.js
+++ lsp/react_native/fokin-weather/App.js
@@ -1,33 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text> Hello </Text>
-      </View>
-    <View style={styles.blueView}>
-      <Text>Hello </Text>
-      </View>
-    </View>
-  );
+  return <Loading/>;
 }
-
-const styles = StyleSheet.create({
-  container: { //모든 공간 사용하게함 
-    flex: 1,
-    backgroundColor: 'black',
-    justifyContent: 'center',
-
-  },
-  yellowView: { //레이아웃 나눔 flex : 1 과 flex : 1은 반반 , flex:1 ,2 면 1/3 , 2/3 이런식으로 차지함
-    flex:1,
-    backgroundColor: 'yellow'
-  },
-  blueView: {
-    flex:1,
-    backgroundColor: 'blue'
-  }
-});
